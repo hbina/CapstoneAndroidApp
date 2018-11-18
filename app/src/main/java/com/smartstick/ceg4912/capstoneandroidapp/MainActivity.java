d92b5fa5f07c54c18866c79f72357c84c40438f1
hbina4326 <hanif.ariffin.4326@gmail.com>
2018-11-18 01:12:41 -0500

    removed unnecessary XMLs

--- app/src/main/java/com/smartstick/ceg4912/capstoneandroidapp/MainActivity.java
+++ app/src/main/java/com/smartstick/ceg4912/capstoneandroidapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.smartstick.ceg4912.capstoneandroidapp;
 
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,22 +20,47 @@ import com.android.volley.toolbox.Volley;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends Activity {
 
-    final static String SMART_STICK_URL = "http://SmartWalkingStick-env.irckrevpyt.us-east-1.elasticbeanstalk.com/path";
-    TextView mTextView;
-    RequestQueue queue;
+    private final static String SMART_STICK_URL = "http://SmartWalkingStick-env.irckrevpyt.us-east-1.elasticbeanstalk.com/path";
+    private TextView debugTextView;
+    private RequestQueue requestQueue;
+    private BluetoothAdapter myBluetooth = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = findViewById(R.id.debug_textview);
-        queue = Volley.newRequestQueue(getApplicationContext());
+        debugTextView = findViewById(R.id.debug_textview);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+
+        if (myBluetooth == null) {
+            Log.d(this.toString(), "No Bluetooth devices available");
+        } else if (!myBluetooth.isEnabled()) {
+            Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnBTon, 1);
+        }
     }
 
+    private void pairedDevicesList() {
+        int counter = 0;
+        if (myBluetooth != null) {
+            Set<BluetoothDevice> pairedDevices = myBluetooth.getBondedDevices();
+            if (pairedDevices.size() > 0) {
+                for (BluetoothDevice bt : pairedDevices) {
+                    Log.d(this.toString(), bt.toString());
+                }
+            } else {
+                Log.d(this.toString(), "No paired Bluetooth devices found");
+            }
+        } else {
+            Log.d(this.toString(), "No Bluetooth devices available");
+        }
+    }
 
     public void onSync(View v) {
         Log.d(this.toString(), "User have clicked onSync");
@@ -40,23 +68,23 @@ public class MainActivity extends Activity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        mTextView.setText("Response is: " + response);
+                        debugTextView.setText("Response is: " + response);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                mTextView.setText("" + error.getMessage());
+                debugTextView.setText("" + error.getMessage());
             }
         }) {
             @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
+            public Map<String, String> getHeaders() {
                 Map<String, String> pars = new HashMap<>();
                 pars.put("Content-Type", "application/x-www-form-urlencoded");
                 return pars;
             }
 
             @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
+            protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<>();
                 params.put("from", "A");
                 params.put("to", "Freedom");
@@ -65,11 +93,12 @@ public class MainActivity extends Activity {
 
         };
         Log.d(this.toString(), jsonObjRequest.toString());
-        queue.add(jsonObjRequest);
+        requestQueue.add(jsonObjRequest);
     }
 
 
     public void onVoice(View v) {
         Log.d(this.toString(), "User have clicked onVoice");
+        pairedDevicesList();
     }
 }
