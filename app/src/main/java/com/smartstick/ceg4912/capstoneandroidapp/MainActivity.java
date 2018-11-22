f7ae07293b54ecc7ce685adca7e4a18ee5f55a55
hbina4326 <hanif.ariffin.4326@gmail.com>
2018-11-21 22:20:55 -0500

    replaced the all ways...

--- app/src/main/java/com/smartstick/ceg4912/capstoneandroidapp/MainActivity.java
+++ app/src/main/java/com/smartstick/ceg4912/capstoneandroidapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -20,6 +21,8 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -28,13 +31,25 @@ import java.util.UUID;
 public class MainActivity extends Activity {
 
     private final static String SMART_STICK_URL = "http://SmartWalkingStick-env.irckrevpyt.us-east-1.elasticbeanstalk.com/path";
+    private final static UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
     private static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private BluetoothDevice device;
     private static String bluetoothAddress = null;
     private static BluetoothSocket bluetoothSocket = null;
     private static boolean isBluetoothConnected = false;
     private static RequestQueue requestQueue;
     private static BluetoothAdapter myBluetooth = null;
-    private static TextView debugTextView;
+    private TextView debugTextView;
+    private boolean stopThread;
+    private byte[] buffer;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+    private final String DEVICE_ADDRESS = "98:D3:31:FC:27:5D";
+    private BluetoothSocket socket;
+    TextView textView;
+    boolean deviceConnected = false;
+    Thread thread;
+    int bufferPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +66,6 @@ public class MainActivity extends Activity {
             Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(turnBTon, 1);
         }
-        new ConnectBT().execute();
     }
 
     @Override
@@ -110,6 +124,7 @@ public class MainActivity extends Activity {
         Log.d(this.toString(), "perform request on link:" + jsonObjRequest.getUrl());
         requestQueue.add(jsonObjRequest);
         sendStringToBluetooth(String.valueOf(System.currentTimeMillis()));
+        beginRequest();
     }
 
 
@@ -181,5 +196,119 @@ public class MainActivity extends Activity {
                 isBluetoothConnected = true;
             }
         }
+    }
+
+    public boolean BluetoothConnect() {
+        boolean connected = true;
+        try {
+            socket = device.createRfcommSocketToServiceRecord(PORT_UUID);
+            socket.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            connected = false;
+        }
+        if (connected) {
+            try {
+                outputStream = socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                inputStream = socket.getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+        return connected;
+    }
+
+    public boolean BluetoothInit() {
+        Log.d(this.toString(), "initializing Bluetooth...");
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Log.d(this.toString(), "device cannot open Bluetooth...");
+            debugTextView.setText(getString(R.string.device_cannot_support_bluetooth));
+            return false;
+        } else {
+            Log.d(this.toString(), "device have opened a Bluetooth...");
+            if (!bluetoothAdapter.isEnabled()) {
+                Log.d(this.toString(), "Bluetooth is not enabled! Requesting permission to enable now...");
+                Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableAdapter, 0);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
+            if (bondedDevices.isEmpty()) {
+                Log.d(this.toString(), "no connected Bluetooth devices found...");
+                debugTextView.setText(getString(R.string.please_pair_device_first));
+            } else {
+                for (BluetoothDevice iterator : bondedDevices) {
+                    Log.d(this.toString(), "found Bluetooth device with address:" + iterator.getAddress());
+                    if (iterator.getAddress().equals(DEVICE_ADDRESS)) {
+                        device = iterator;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    public void beginRequest() {
+        stopThread = true;
+        Log.d(this.toString(), "begin connecting to Arduino...");
+        if (BluetoothInit()) {
+            if (BluetoothConnect()) {
+                deviceConnected = true;
+                beginListenForData();
+                debugTextView.append(getString(R.string.connection_opened));
+            } else {
+                Log.d(this.toString(), "unable to establish connection with Bluetooth device...");
+            }
+
+        } else {
+            Log.d(this.toString(), "fail to initialize Bluetooth...");
+        }
+    }
+
+    private void beginListenForData() {
+        Log.d(this.toString(), "Begin listening to data...");
+        final Handler handler = new Handler();
+        stopThread = false;
+        buffer = new byte[1024];
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                Log.d(this.toString(), "Begin running thread..");
+                while (!Thread.currentThread().isInterrupted() && !stopThread) {
+                    try {
+                        int byteCount = inputStream.available();
+                        if (byteCount > 0) {
+                            Log.d(this.toString(), "byteCount is larger than 0");
+                            byte[] rawBytes = new byte[byteCount];
+                            inputStream.read(rawBytes);
+                            final String string = new String(rawBytes, "UTF-8");
+                            handler.post(new Runnable() {
+                                public void run() {
+                                    Log.d(this.toString(), "Received the following string:" + string);
+                                    debugTextView.setText(string);
+                                }
+                            });
+
+                        }
+                    } catch (IOException ex) {
+                        Log.d(this.toString(), "error:" + ex.getMessage());
+                        stopThread = true;
+                    }
+                }
+            }
+        });
+        thread.start();
     }
 }
